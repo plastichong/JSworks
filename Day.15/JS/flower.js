6b5c4ba4b58bacdc52cebe21785b6442f386b5e1
plastichong <zakkslash18@naver.com>
2022-05-27 18:18:34 +0900

    15일차 자바스크립트

---
+++ Day.15/JS/flower.js
@@ -0,0 +1,10 @@
+
+        function showText(){
+            document.getElementById("detail").style.display = "block";
+            document.getElementById("show").style.display = "none";
+        }
+
+        function hideText(){
+                document.getElementById("detail").style.display = "none";
+                document.getElementById("show").style.display = "block";
+        }
